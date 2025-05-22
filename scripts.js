0aea5908b09831340f6ada7ec8be2367fdb9672e
jotapao <joaopedrocordeiro12.jops@gmail.com>
2025-05-22 14:02:23 -0300

    Formatando o input para aceitar apenas números

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 //seleciona os elementos do formulário
 const amount = document.getElementById("amount")
+
+
+//formatano o input para aceitar apenas números
 amount.oninput = () => {
+    let value = amount.value.replace(/\D/g, "")
+    amount.value = value
 
 }
